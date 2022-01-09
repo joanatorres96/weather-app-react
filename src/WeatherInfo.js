a458d088cb0336054583e0ff657bc8c317acf851
Joana Torres <joanatorres96@gmail.com>
2022-01-09 16:05:51 +0000

    adjust the page for different devices

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -8,17 +8,18 @@ export default function WeatherInfo(props){
 return(
 <div className="WeatherInfo">
 <h5 className="mt-2"><CurrentDate date={props.info.date} /></h5> 
-<div className="row mt-4 mb-4">
-<div className="col-sm-4 p-0  ms-4 text-end">
+<div className="row my-4 mx-1">
+
+<div className="col-sm-5 p-0 me-5 text-end" id="principalInfo">
     <h1>{props.info.city}</h1>
     <WeatherTemp celsius={props.info.temperature} />
     
    </div>
-   <div className="col-sm-1 mx-3 p-0 mt-1 text-center"> 
+   <div className="col-sm-1 p-0 mt-3 text-center"> 
      <Icon icon={props.info.icon} />
      </div>
 
-<div className="col-sm-3 p-0 mt-2 me-2 text-start " >       
+<div className="col-sm-3 p-0 mt-3 ms-1 text-start " id="details" >       
     <ul>
         <li className="weather-info"><h5 className="text-capitalize">{`${props.info.description}`}</h5></li>
         <li className="weather-info">{`Humidity: ${props.info.humidity}%`}</li>
